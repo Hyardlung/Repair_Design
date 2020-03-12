8dffbdd4eccc250d1effe24412304bb1303c60db
Dmitry Volkov <hyardlu@protonmail.com>
2020-03-12 23:08:42 +0700

    feat: added closing function of modal window on click to esc key and empty space

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,38 @@
-/*
+
 document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector('.modal');
+  const modalDialog = document.querySelector('.modal__dialog');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
+  const closeModal = document.querySelector('.modal');
   const switchModal = () => {
     modal.classList.toggle('modal_visible');
   }
+  const closeModalEsc = () => {
+    modal.classList.remove('modal_visible');
+  }
+
   modalBtn.forEach(element => {
     element.addEventListener('click', switchModal);
   });
-
   closeBtn.addEventListener('click', switchModal);
+
+  closeModal.addEventListener('click', function(e) {
+    if (e.target === modal && e.target != modalDialog) {
+      closeModalEsc();
+    }
+  });
+
+  document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 27) {
+      closeModalEsc();
+    }
+  });
 });
-*/
 
 
+
+/*
 $(document).ready(function () {
   let modal = $('.modal'),
       modalBtn = $('[data-toggle=modal]'),
@@ -27,3 +45,4 @@ $(document).ready(function () {
     modal.toggleClass('modal_visible');
   });
 });
+*/
