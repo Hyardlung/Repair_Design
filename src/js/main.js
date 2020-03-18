8c573583a5a2c831757d7daf7d9bbcfbfb8f729a
Dmitry Volkov <hyardlu@protonmail.com>
2020-03-18 23:38:12 +0700

    feat: added validate for modal__form

--- src/js/main.js
+++ src/js/main.js
@@ -92,4 +92,37 @@ $(document).ready(function () {
     }
     )
   new WOW().init();
+
+  //Валидация форм
+  $('.modal__form').validate({
+    errorClass: "invalid",
+    rules: {
+      // строчное правило
+      userName: {
+        required: true,
+        minlength: 2
+      },
+      userPhone: "required",
+      // правило-объект
+      userEmail: {
+        required: true,
+        email: true
+      }
+    }, //сообщения
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Имя не короче двух букв"
+      }, 
+      userPhone: "Телефон обязателен",
+      userEmail: {
+        required: "Обязательно укажите email",
+        email: "Укажите в формате: name@domain.com"
+      }
+    }  
+  });
+
+  // Маска для номера телефона
+  $('[type=tel]').mask('+7 (000) 000-00-00', {placeholder: "+7 (___) ___-__-__"});
+
 });
