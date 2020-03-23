c18c30a299451daa4351aed9a89cf0e56b145bc3
Mitrey Hurd <51011143+Hyardlung@users.noreply.github.com>
2020-03-23 17:42:03 +0700

    feat: also added php script for control__form and footer__form

---
+++ src/control.php
@@ -0,0 +1,39 @@
+<?php
+
+
+$userName = $_POST['userNameControl'];
+$userPhone = $_POST['userPhoneControl'];
+
+// Load Composer's autoloader
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'ssl://smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'hyardlung@gmail.com';                     // SMTP username
+    $mail->Password   = 'SiDD0804';                               // SMTP password
+    $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('hyardlung@gmail.com', 'Dmitry');      //ящик, с которого приходят письма клиентам
+    $mail->addAddress('mitrey.hurd@ya.ru');     // почта заказчика, куда падают заявки с сайта (например)
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка с сайта';
+    $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}";
+
+    $mail->send();
+    header('Location: thanks.html');      //куда пользователь попадёт после отправки формы
+} catch (Exception $e) {
+    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
